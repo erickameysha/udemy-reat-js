cb63302b47c65c22172c9c6f5d9044dbe5a587da
Eric <ekameyshaaa@gmail.com>
2021-10-11 13:26:36 +0300

    методы и свойства строк

---
+++ js/01.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const str = 'teSt'
+const arr = [1, 2, 3]
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+let fruit = 'Some fruit'
+console.log(fruit.indexOf('fruit'));
+ 
+
+const logg = "hello world";
+
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6,11));
+console.log(logg.substr(6,5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const text = "12.2px";
+console.log(parseInt(text));
+console.log(parseFloat(text));
