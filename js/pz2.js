093e88c1333ae89ef3cb998b373141f4a2d92609
Eric <ekameyshaaa@gmail.com>
2021-10-26 16:52:41 +0300

    pz 1-3

---
+++ js/pz2.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+if (personalMovieDB.count < 10) {
+    console.log('Посмотрели довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель ');
+} else if (personalMovieDB.count >= 30) {
+    console.log("киноман");
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
